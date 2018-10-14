da09075e10c59a65ebb8db62637fa0a1e755f6a9
Marcus Washington <marcus.washington@me.com>
2018-10-13 21:23:19 -0500

    Update to Team 8648 TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team8648HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team8648HardwarePushbot.java
@@ -8,17 +8,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Team8648HardwarePushbot {
 
     /* Public OpMode members. */
-    public DcMotor leftDrive   = null;
+    public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
-    public DcMotor  mainArm     = null;
-    public DcMotor  slideArm    = null;
+    public DcMotor  lift     = null;
+    public DcMotor  arm    = null;
+    public DcMotor  armRaise = null;
 
-    public Servo leftClaw    = null;
-    public Servo    rightClaw   = null;
-    public Servo    longReclicClaw    = null;
-    public Servo    shortReclicClaw   = null;
-    public Servo    colorSensorServo  = null;
-    public Servo    hitBallServo      = null;
+    public Servo    armServo    = null;
+
 
 
     public static final double MID_SERVO       =  0.25;
@@ -40,10 +37,11 @@ public class Team8648HardwarePushbot {
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        mainArm    = hwMap.get(DcMotor.class, "main_arm");
-        slideArm   = hwMap.get(DcMotor.class, "slide");
+        leftDrive  = hwMap.get(DcMotor.class, "LeftMotor");
+        rightDrive = hwMap.get(DcMotor.class, "RightMotor");
+        lift    = hwMap.get(DcMotor.class, "LiftMotor");
+        arm   = hwMap.get(DcMotor.class, "ArmMotor");
+        armRaise = hwMap.get(DcMotor.class, "ArmRaiseMotor");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
@@ -52,31 +50,24 @@ public class Team8648HardwarePushbot {
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        mainArm.setPower(0);
-        slideArm.setPower(0);
+        lift.setPower(0);
+        arm.setPower(0);
+        armRaise.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        mainArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        slideArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armRaise.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         // Define and initialize ALL installed servos.
-        leftClaw  = hwMap.get(Servo.class, "left_hand");
-        rightClaw = hwMap.get(Servo.class, "right_hand");
-        longReclicClaw  = hwMap.get(Servo.class, "long_relic_claw");
-        shortReclicClaw = hwMap.get(Servo.class, "short_relic_claw");
-        //colorSensorServo = hwMap.get(Servo.class, "color_sensor_servo");
-        //hitBallServo = hwMap.get(Servo.class, "hit_ball_servo");
+        armServo  = hwMap.get(Servo.class, "ArmServo");
 
 
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
-        longReclicClaw.setPosition(0);
-        shortReclicClaw.setPosition(0);
-        //colorSensorServo.setPosition(0);
-        //hitBallServo.setPosition(.50);
+        armServo.setPosition(0);
+
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team8648PushbotTeleOp.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Pushbot: Team8648 Pushbot Teleop", group="Pushbot")
+public class Team8648PushbotTeleOp extends OpMode {
+
+    Team8648HardwarePushbot robot = new Team8648HardwarePushbot();
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+        double armMotor;
+        double armRaiseMotor;
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        //arm
+        armMotor = -gamepad2.left_stick_y;
+        armRaiseMotor = -gamepad2.right_stick_y;
+
+
+
+        robot.leftDrive.setPower(left);
+        robot.rightDrive.setPower(right);
+        robot.arm.setPower(armMotor);//changed**
+        robot.armRaise.setPower(armRaiseMotor);//changed***
+
+
+
+        // Use gamepad buttons to move the arm up (Y) and down (A)
+        if (gamepad1.y)
+            robot.lift.setPower(1);
+        else if (gamepad1.a)
+            robot.lift.setPower(-0.01);
+        else
+            robot.lift.setPower(0.2);
+
+
+        //Slide Arm Controller buttons??????
+        //i was told not to use this on 1-13-18
+        if(gamepad2.a)
+            robot.armServo.setPosition(1);
+        else if (gamepad2.y)
+            robot.armServo.setPosition(0);
+
+
+
+        // Send telemetry message to signify robot running;
+        //telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+        //telemetry.addData("left",  "%.2f", left);
+        //telemetry.addData("right", "%.2f", right);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+
+}
